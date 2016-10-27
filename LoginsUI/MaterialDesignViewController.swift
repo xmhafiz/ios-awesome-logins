aa5b9fd5f4b55debdf2415c0452a56388ff5bd36
h44f33z <h44f33z@gmail.com>
2016-10-27 13:17:09 +0800

    fix buttons constraint

---
+++ LoginsUI/MaterialDesignViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MaterialDesignViewController.swift
+//  LoginsUI
+//
+//  Created by Hafiz on 27/10/2016.
+//  Copyright © 2016 Hafiz. All rights reserved.
+//
+
+import UIKit
+
+class MaterialDesignViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
